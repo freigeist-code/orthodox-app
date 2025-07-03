b7ecdde4b8ebfd17568c3f6e0c91443a66318db7
freigeist-code <jacobfrey31@gmail.com>
2025-07-03 03:54:14 -0500

    Update server.js

--- js/server.js
+++ js/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Use native fetch in Node 18+, or require('node-fetch') for older Node
 let fetch;
